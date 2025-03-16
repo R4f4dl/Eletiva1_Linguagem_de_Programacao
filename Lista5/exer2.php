40be3d7e6bd4f4cd47e8970a842013a52a305f06
R4f4dl <rafaeldaunlopes@gmail.com>
2025-03-16 18:44:29 -0300

    Lista 5 Exer2

---
+++ Lista5/exer2.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 1</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <?php for($i=0;$i<5;$i++): ?> 
+            <input type="text" name="nome[]" placeholder="Nome"/>
+            <input type="number" name="n1[]" placeholder="Nota 1:"/>
+            <input type="number" name="n2[]" placeholder="Nota 2:"/>
+            <input type="number" name="n3[]" placeholder="Nota 3:"/>
+            <p> </p>
+        <?php endfor; ?>
+        <p></p>
+        <button type="submit">Enviar</button>
+    </form>
+
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] == "POST"){
+            try{
+                $aluno = array();
+                $nome = $_POST['nome'];
+                $n1 = $_POST['n1']; 
+                $n2 = $_POST['n2'];
+                $n3 = $_POST['n3'];
+
+
+                for($i=0;$i<count($nome);$i++){
+                    $no1 = $n1[$i];
+                    $no2 = $n1[$i];
+                    $no3 = $n1[$i];
+                    $media  = ($no1+$no2+$no3)/3;
+                    $chavenome = $nome[$i];
+                    $aluno[$chavenome] = $media;
+                }
+
+                arsort($aluno); //Serve para organizar em ordem alfabetica
+                foreach ($aluno as $nome => $media) {
+                    echo "Nome: $nome - Média: $media<br>";
+                }
+
+
+            } catch (Exception $e){
+                echo $e->getMessage();
+            }
+        }
+    ?>
+</body>
+</html>
